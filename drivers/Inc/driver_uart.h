1250325711d172cf6e52427ea74a7526ac91c5d0
slemanz <william_sleman@hotmail.com>
2024-12-28 14:39:16 -0300

    project(feat): add example hello.c and delete some files

--- drivers/Inc/driver_uart.h
+++ drivers/Inc/driver_uart.h
@@ -6,6 +6,6 @@
 
 void uart2_init(void);
 void uart2_write_byte(uint8_t data);
-
+void uart2_init_pins(void);
 
 #endif
--- drivers/Src/driver_uart.c
+++ drivers/Src/driver_uart.c
@@ -24,3 +24,18 @@ void uart2_write_byte(uint8_t data)
 	while(!(UART2->SR & (1 << 7)));
     UART2->DR = data;
 }
+
+void uart2_init_pins(void)
+{
+    GPIO_Handle_t UartPin;
+
+    UartPin.pGPIOx = GPIOA;
+    UartPin.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+    UartPin.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_2;
+    UartPin.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+    UartPin.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+    UartPin.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+    UartPin.GPIO_PinConfig.GPIO_PinAltFunMode = PA2_ALTFN_UART2_TX;
+
+    GPIO_Init(&UartPin);
+}
