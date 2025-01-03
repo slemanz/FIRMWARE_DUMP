a40ce59373f7ddbacc3014e81762439e40a822ab
slemanz <william_sleman@hotmail.com>
2025-01-03 04:20:49 -0300

    example(feat)

--- app/Src/main.c
+++ app/Src/main.c
@@ -29,9 +29,25 @@ int main(void)
     uart2_init_pins();
     uart2_init();
 
+    int32_t *val1 = (int32_t *)0x20001000L;
+    int32_t *val2 = (int32_t *)0x20001004L;
+    int32_t *res =  (int32_t *)0x20001008L;
+
+    *val1 = 12;
+    *val2 = 8;
+    *res  = 0;
+
+    __asm volatile("LDR R1, =#0x20001000");
+    __asm volatile("LDR R2, =#0x20001004");
+    __asm volatile("LDR R3, =#0x20001008");
+    __asm volatile("LDR R0, [R1]");
+    __asm volatile("LDR R1, [R2]");
+    __asm volatile("ADD R0, R0, R1");
+    __asm volatile("STR R0, [R3]");
+
     while (1)
     {
-        printf("Hello world!\n");
-        delay_cycles(16000000/4);
+        printf("%ld + %ld = %ld\n", *val1, *val2, *res);
+        delay_cycles(16000000);
     }
 }
