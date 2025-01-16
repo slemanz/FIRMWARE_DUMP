ed6be6965567e130129286134201ad2eb2fa06a0
slemanz <william_sleman@hotmail.com>
2025-01-16 04:02:54 -0300

    task(feat)

--- MASTER1/notes/13_task.md
+++ MASTER1/notes/13_task.md
@@ -1,2 +1,11 @@
 # 13. Task Schedular
 
+## Implementing a scheduler 
+
+- Let's implement a scheduler which schedules multiple user tasks in a round-robin fashion by carrying out the context switch operation 
+- Round robin scheduling method is, time slices are assigned to each task in equal portions and in circular order
+
+## What is a task? 
+- A task is nothing but a piece of code, or you can call it a ('C') function, which does a specific job when it is allowed to run on the CPU. 
+- A task has its own stack to create its local variables when it runs on the CPU. Also when scheduler decides to remove a task from CPU, scheduler first saves the context(state) of the task in task's private stack 
+-  So, in summary, a piece code or a function is called a task when it is schedulable and never loses its 'state' unless it is deleted permanently.
--- MASTER1/task/Src/main.c
+++ MASTER1/task/Src/main.c
@@ -1,11 +1,6 @@
 #include "stm32f401xx.h"
 #include <stdio.h>
 
-/*
-    Exemple: Load 2 values from memory, add them and store the result back to 
-    the memory using inline assembly statements.
- */
-
 void uart2_init_pins(void);
 
 extern int __io_putchar(int ch)
@@ -23,31 +18,57 @@ void delay_cycles(uint32_t cycles)
     }
 }
 
+void task1_handler(void);
+void task2_handler(void);
+void task3_handler(void);
+void task4_handler(void);
+
+
 int main(void)
  {
 
     uart2_init_pins();
     uart2_init();
 
-    int32_t *val1 = (int32_t *)0x20001000L;
-    int32_t *val2 = (int32_t *)0x20001004L;
-    int32_t *res =  (int32_t *)0x20001008L;
-
-    *val1 = 12;
-    *val2 = 8;
-    *res  = 0;
-
-    __asm volatile("LDR R1, =#0x20001000");
-    __asm volatile("LDR R2, =#0x20001004");
-    __asm volatile("LDR R3, =#0x20001008");
-    __asm volatile("LDR R0, [R1]");
-    __asm volatile("LDR R1, [R2]");
-    __asm volatile("ADD R0, R0, R1");
-    __asm volatile("STR R0, [R3]");
-
     while (1)
     {
         printf("%ld + %ld = %ld\n", *val1, *val2, *res);
         delay_cycles(16000000);
+    }
+}
+
+void task1_handler(void)
+{
+    printf("This is task 1\n");
+    while(1)
+    {
+
+    }
+}
+
+void task2_handler(void)
+{
+    printf("This is task 2\n");
+    while(1)
+    {
+
+    }
+}
+
+void task3_handler(void)
+{
+    printf("This is task 3\n");
+    while(1)
+    {
+
+    }
+}
+
+void task4_handler(void)
+{
+    printf("This is task 4\n");
+    while(1)
+    {
+
     }
 }
